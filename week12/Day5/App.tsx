690b83e4966a066c864a3457540c525732a1ecb7
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-13 21:44:45 +0900

    Clock, TodoList import

---
+++ week12/Day5/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TodoList from './Todolist';
+import Clock from './Timer';
+
+function App() {
+  return (
+    <div className="container">
+      <TodoList />
+      <Clock />
+    </div>
+  );
+}
+
+export default App;
