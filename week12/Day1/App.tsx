4dd64fedd2a46d7172c0cc700c7177b0d88bd854
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-05-13 22:46:42 +0900

    Create App.tsx

---
+++ week12/Day1/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import TodoList from './Todolist';
+import Clock from './Timer';
+
+function App() {
+  return (
+    <div className="container">
+      <TodoList />
+      <Clock />
+    </div>
+  );
+}
+
+export default App;
